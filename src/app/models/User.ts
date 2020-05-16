e2b80f457a9e392e231d4dec3bbd8e5c372a1894
João Pedro <johnsj.p@hotmail.com>
2020-05-16 19:27:02 -0300

    create user model

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -4,13 +4,9 @@ export class UserSchema {
   name: string;
   age: number;
 
-  created_at?: Date = new Date();
-  updated_at?: Date = new Date();
-
-  public constructor(name: string, age: number, updated_at?: Date) {
+  public constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
-    this.updated_at = updated_at;
   }
 }
 
